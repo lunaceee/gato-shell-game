548b739acffe9205f42a2550e1709cbfc375c571
lunaceee <lunaceee@gmail.com>
2019-09-29 19:32:15 -0700

    tweak eye position

--- index.js
+++ index.js
@@ -31,20 +31,22 @@
 
   const gato = new SpriteGroup();
   gato.add(".body", 0, 0);
-  gato.add(".default-eye-frame", 2.8, 3);
+  gato.add(".default-eye-frame", 2.6, 3);
   gato.add(".eye-background", 2.9, 3);
   gato.add(".eye-left", 0, 0, 2.5, 2);
   gato.add(".eye-right", 0, 0, 2.5, 2);
   gato.add(".eye-dots-default-left", 2, 5);
   gato.add(".eye-dots-default-right", 9, 5);
 
+  gato.add(".shuffling-eye-frame", 2.6, 3);
   gato.add(".eye-left-down", 5, 4.5);
-  gato.add(".eye-right-down", 10.2, 4.5);
+  gato.add(".eye-right-down", 10, 4.5);
   gato.add(".eye-dots-down-left", 6.2, 6);
-  gato.add(".eye-dots-down-right", 11, 6);
+  gato.add(".eye-dots-down-right", 11, 5.8);
 
   gato.add(".funky-eye-frame", 2.8, 3);
   gato.add(".funky-eyes", 4.6, 4.3);
+
   gato.add(".tail-up", 10.5, 9.5);
   gato.add(".legs-center", 5, 16);
   gato.add(".legs-default", 5, 16);
@@ -53,7 +55,6 @@
   gato.add(".mustache", -1, 8);
   gato.add(".nose-mouth-default", 8, 9);
   gato.add(".nose-mouth-snarky", 8, 9);
-  gato.add(".shuffling-eye-frame", 2.8, 3);
 
   const startButton = document.querySelector(".start-button");
 
